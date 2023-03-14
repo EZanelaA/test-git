ad90f56c1b56fd63a530620f28162e59db3b0e1e
Eduardo Z. A <z9.edu@hotmail.com>
2023-03-14 00:35:31 -0300

    Using functions to output 'hello world'

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+void printOutString(std::string);
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  std::string hello_world = "Hello, World!";
+  printOutString(hello_world);
   return 0;
+}
+
+void printOutString(std::string str) {
+  std::cout << str << std::endl;
 }
